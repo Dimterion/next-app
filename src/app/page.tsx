12e67542fb243e978d339ceec8df111154025228
Dmitrii <dimterion59@gmail.com>
2025-07-02 12:05:57 +0200

    add home page elements & style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Next App</h1>
-      <p>Lorem ipsum</p>
-      <Link href="/">Home</Link>
-      <Image
-        src="/home_page_img.jpg"
-        alt="Home page image"
-        width={100}
-        height={100}
-        priority={true}
-      />
+      <section className="mx-auto flex max-w-7xl flex-col-reverse items-center justify-between gap-8 px-6 py-12 md:flex-row">
+        <article className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Next App
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">Lorem Ipsum</h1>
+          <p className="mb-10 text-lg text-gray-600">Lorem ipsum dolor.</p>
+          <Link
+            href="/"
+            className="border-2 border-black bg-white px-6 py-3 text-black transition duration-100 hover:bg-black hover:text-white"
+          >
+            Next
+          </Link>
+        </article>
+        <Image
+          src="/home_page_img.jpg"
+          alt="Home page image"
+          width={350}
+          height={350}
+        />
+      </section>
     </main>
   );
 }
