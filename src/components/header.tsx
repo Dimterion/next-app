cb76765c11083c4ac09baed92e5939ec464f6f92
Dmitrii <dimterion59@gmail.com>
2025-07-03 11:41:06 +0200

    add header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex flex-row gap-2">
-      <Link href="/">Next App</Link>
-      <nav>
+    <header className="flex w-full items-center justify-between bg-white px-6 py-4">
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        className="w-[50px] md:w-[150px]"
+        width={150}
+        height={50}
+      />
+      <nav className="flex items-center gap-2.5">
+        <Link href="/">Home</Link>
         <Link href="/about">About</Link>
       </nav>
     </header>
